8563608c4e68998889984efbf6c0b2fe727cc01a
Keyi Zhang <keyi@stanford.edu>
2020-01-05 22:22:16 -0800

    update result_of since it's deprecated

--- src/cxxpool.h
+++ src/cxxpool.h
@@ -2,6 +2,7 @@
 // Version: 2.0.0
 // Repository: https://github.com/bloomen/cxxpool
 // Copyright: 2015 Christian Blume
+//            2019-2020 Keyi Zhang
 // License: http://www.opensource.org/licenses/mit-license.php
 #pragma once
 #include <thread>
@@ -18,12 +19,73 @@
 
 namespace cxxpool {
 namespace detail {
+// copied from here
+// https://en.cppreference.com/w/cpp/types/result_of
+// since result_of is deprecated in C++20
+template <class T>
+struct is_reference_wrapper : std::false_type {};
+template <class U>
+struct is_reference_wrapper<std::reference_wrapper<U>> : std::true_type {};
 
+template<class T>
+struct invoke_impl {
+    template<class F, class... Args>
+    static auto call(F&& f, Args&&... args)
+    -> decltype(std::forward<F>(f)(std::forward<Args>(args)...));
+};
+
+template<class B, class MT>
+struct invoke_impl<MT B::*> {
+    template<class T, class Td = typename std::decay<T>::type,
+        class = typename std::enable_if<std::is_base_of<B, Td>::value>::type
+    >
+    static auto get(T&& t) -> T&&;
+
+    template<class T, class Td = typename std::decay<T>::type,
+        class = typename std::enable_if<is_reference_wrapper<Td>::value>::type
+    >
+    static auto get(T&& t) -> decltype(t.get());
+
+    template<class T, class Td = typename std::decay<T>::type,
+        class = typename std::enable_if<!std::is_base_of<B, Td>::value>::type,
+        class = typename std::enable_if<!is_reference_wrapper<Td>::value>::type
+    >
+    static auto get(T&& t) -> decltype(*std::forward<T>(t));
+
+    template<class T, class... Args, class MT1,
+        class = typename std::enable_if<std::is_function<MT1>::value>::type
+    >
+    static auto call(MT1 B::*pmf, T&& t, Args&&... args)
+    -> decltype((invoke_impl::get(std::forward<T>(t)).*pmf)(std::forward<Args>(args)...));
+
+    template<class T>
+    static auto call(MT B::*pmd, T&& t)
+    -> decltype(invoke_impl::get(std::forward<T>(t)).*pmd);
+};
+
+template<class F, class... Args, class Fd = typename std::decay<F>::type>
+auto INVOKE(F&& f, Args&&... args)
+-> decltype(invoke_impl<Fd>::call(std::forward<F>(f), std::forward<Args>(args)...));
+
+template <typename AlwaysVoid, typename, typename...>
+struct invoke_result { };
+template <typename F, typename...Args>
+struct invoke_result<decltype(void(detail::INVOKE(std::declval<F>(), std::declval<Args>()...))),
+    F, Args...> {
+    using type = decltype(detail::INVOKE(std::declval<F>(), std::declval<Args>()...));
+};
+
+// Minimal C++11 implementation:
+template <class> struct result_of;
+template <class F, class... ArgTypes>
+struct result_of<F(ArgTypes...)> {
+    using type = decltype(detail::INVOKE(std::declval<F>(), std::declval<ArgTypes>()...));
+};
 
 template<typename Iterator>
 struct future_info {
     typedef typename std::iterator_traits<Iterator>::value_type future_type;
-    typedef typename std::result_of<decltype(&future_type::get)(future_type)>::type value_type;
+    typedef typename result_of<decltype(&future_type::get)(future_type)>::type value_type;
     static constexpr bool is_void = std::is_void<value_type>::value;
 };
 
